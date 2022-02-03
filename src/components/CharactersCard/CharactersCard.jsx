43a59dd3a47f7623e52d594363e86941cd956e31
Sergio González Gómez <sergiogonzalezgomez15@gmail.com>
2022-02-03 15:05:57 +0100

    charactersStyles

--- src/components/CharactersCard/CharactersCard.jsx
+++ src/components/CharactersCard/CharactersCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Link, generatePath} from 'react-router-dom';
+import './CharactersCard.scss';
 
 export const CharactersCard = ({character}) => {
   console.log(character);
   character.image = character.name === "Oberyn Martell" ?   "./images/oberyn-martell.webp" : character.image;
   character.image = character.name === "Grenn" ?   "./images/grenn.webp" : character.image;
-  return <div>
+  return <div className="charactercard-container">
     <Link to={generatePath("/characters/:name", {name: character.name})}>
-    <img src={character.image} alt={character.name} />
+    <img className="charactercard-img" src={character.image} alt={character.name} />
     </Link>
     </div>;
 };
--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect }from 'react';
 import {getCharacters} from '../../api/fetchToApi';
-import { CharactersCard } from '../../components/CharactersCard/CharactersCard'
-
+import { CharactersCard } from '../../components/CharactersCard/CharactersCard';
+import './Characters.scss';
 
 export const Characters = () => {
 
@@ -10,7 +10,7 @@ export const Characters = () => {
     getCharacters().then((data) => setCharacters(data))
   },[])
   
-  return <div>
+  return <div className="characters-list">
       {characters.map((character) => {
         return <CharactersCard key={character.id} character={character} />
       })}
