91906ef7f1b612ab3afac743d404f54c55f0a83c
alejandro martin rodriguez <thalitaa@hotmail.com>
2022-02-05 17:24:20 +0000

    add Styles en components

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Context } from "../../App";
 import {Link} from 'react-router-dom';
+import "./BackButton.scss"
 
 export const BackButton = () => {
   const {page} = React.useContext(Context);
@@ -8,6 +9,6 @@ export const BackButton = () => {
   fatherPage = page === "CharactersDetail" ? "characters" : "houses"
 
   return <div className="backButton">
-      <Link to={`/${fatherPage}`}><img src="/images/back.png" alt="back" /> Back</Link>
+  <Link to={`/${fatherPage}`}><img className="backButton-img" src="/images/back.png" alt="back" /><p className="backButton-txt">Volver</p></Link>
   </div>;
 };
